2c127d123cd9ca0ea65b49a522c19fbf29075e00
YOGENDRA KUMAR <yogendrakumar8219@gmail.com>
2023-10-30 11:09:46 +0530

    commit

--- BusinessLogicsLayer/ServiceRegistration.cs
+++ BusinessLogicsLayer/ServiceRegistration.cs
@@ -61,6 +61,13 @@ namespace BusinessLogicsLayer
             services.AddTransient<IUserProfileBL, UserProfileBL>();
             services.AddTransient<IUserProfileDB, UserProfileDB>();
 
+
+            services.AddTransient<IBasicDetailBL, BasicDetailBL>();
+            services.AddTransient<IBasicDetailDB, BasicDetailDB>();
+
+            services.AddTransient<IBasicDetailTempBL, BasicDetailTempBL>();
+            services.AddTransient<IBasicDetailTempDB, BasicDetailTempDB>();
+
             //services.AddDbContext<ApplicationDbContext>(opt => opt.UseSqlServer("Server=SDG20\\SQLEXPRESS; Database=AFSAC;Integrated Security=True;MultipleActiveResultSets=True;TrustServerCertificate=True"));
             //services.AddDbContext<ApplicationDbContext>(opt => opt.UseSqlServer("Server=.\\sqlexpress; Database=AFSAC;User Id=sa; Password=Admin@2018;MultipleActiveResultSets=True;TrustServerCertificate=True"));
             // services.AddDbContext<ApplicationDbContext>(opt => opt.UseSqlServer("Server=192.168.10.63; database=AFSAC_New; User Id=sa; Password=Admin@2018;Connect Timeout=30;TrustServerCertificate=True; MultipleActiveResultSets=true"));
