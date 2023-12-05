cb8be92f261621bce942f779fa2c40cd212d0c19
master <master@SDG20>
2023-12-05 16:40:36 +0530

    commit

--- BusinessLogicsLayer/ServiceRegistration.cs
+++ BusinessLogicsLayer/ServiceRegistration.cs
@@ -85,7 +85,9 @@ namespace BusinessLogicsLayer
             services.AddTransient<iGetTokenBL, GetTokenBL>();
             services.AddTransient<IStepCounterBL, StepCounterBL>();
             services.AddTransient<ITrnFwnBL, TrnFwnBL>();
+
             services.AddTransient<ITrnICardRequestBL, TrnICardRequestBL>();
+            services.AddTransient<ITrnICardRequestDB, TrnICardRequestDB>();
 
             services.AddTransient<IAPIDataBL, APIDataBL>();
             services.AddTransient<IAPIDataDB, APIDataDB>();
