1bb9f47285e4be376d947305ba090ce1eac8df65
master <master@SDG20>
2023-12-14 17:23:54 +0530

    Landing Page

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -4,11 +4,19 @@ namespace Web.Controllers
 {
     public class HomeController : Controller
     {
+        public IActionResult Index()
+        {
+            return View();
+        }
         public IActionResult Dashboard()
         {
             return View();
         }
         public IActionResult MyTask()
+        {
+            return View();
+        }
+        public IActionResult Request()
         {
             return View();
         }
