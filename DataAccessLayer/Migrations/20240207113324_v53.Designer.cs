d383399940d81a79bb037d028ab2ad2e91a607b0
YOGENDRA KUMAR <yogendrakumar8219@gmail.com>
2024-02-07 17:34:09 +0530

    commit

--- DataAccessLayer/Migrations/20240207113324_v53.Designer.cs
+++ DataAccessLayer/Migrations/20240207113324_v53.Designer.cs
@@ -4,6 +4,7 @@ using DataAccessLayer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DataAccessLayer.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240207113324_v53")]
+    partial class v53
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -1262,8 +1265,6 @@ namespace DataAccessLayer.Migrations
 
                     b.HasIndex("ApplyForId");
 
-                    b.HasIndex("RankId");
-
                     b.HasIndex("RegistrationId");
 
                     b.HasIndex("TypeId");
@@ -2479,12 +2480,6 @@ namespace DataAccessLayer.Migrations
                         .OnDelete(DeleteBehavior.Restrict)
                         .IsRequired();
 
-                    b.HasOne("DataTransferObject.Domain.Master.MRank", "Rank")
-                        .WithMany()
-                        .HasForeignKey("RankId")
-                        .OnDelete(DeleteBehavior.Restrict)
-                        .IsRequired();
-
                     b.HasOne("DataTransferObject.Domain.Master.MRegistration", "Registration")
                         .WithMany()
                         .HasForeignKey("RegistrationId")
@@ -2507,8 +2502,6 @@ namespace DataAccessLayer.Migrations
                     b.Navigation("MApplyFor");
 
                     b.Navigation("MICardType");
-
-                    b.Navigation("Rank");
 
                     b.Navigation("Registration");
                 });
