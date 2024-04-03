eb6f11fcb5bf47526dde6a21e5fc38908404f1f2
kapoor <kapoor2q@gmail.com>
2024-04-03 11:41:02 +0530

    commit

--- BusinessLogicsLayer/API/APIBL.cs
+++ BusinessLogicsLayer/API/APIBL.cs
@@ -26,10 +26,10 @@ namespace BusinessLogicsLayer.API
         //public const string ApiUrl = "http://192.168.10.200/api/";
         //public const string ApiUrl = "https://131.3.47.13:7002/api/";
         //public const string ApiUrloffrs = "https://131.3.47.13:7002/api/";
-          public const string ApiUrl = "http://192.168.10.40:7002/api/";
-          public const string ApiUrloffrs = "http://192.168.10.40:7002/api/";
-        //public const string ApiUrl = "https://192.168.10.203:8443/api/";
-        //public const string ApiUrloffrs = "https://192.168.10.203:8443/api/";
+        //public const string ApiUrl = "http://192.168.10.40:7002/api/";
+        //public const string ApiUrloffrs = "http://192.168.10.40:7002/api/";
+        public const string ApiUrl = "https://192.168.10.203:8443/api/";
+        public const string ApiUrloffrs = "https://192.168.10.203:8443/api/";
         //public const string ApiUrl = "http://192.168.10.40:7002/api/";
         // public const string ApiUrloffrs = "http://192.168.10.40:7002/api/";
         //public const string ApiUrl = "http://131.3.47.13:7002/api/";
