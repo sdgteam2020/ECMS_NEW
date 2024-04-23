a6d4fd1665081a56f592312a9726be416400ddf7
YOGENDRA KUMAR <yogendrakumar8219@gmail.com>
2024-04-23 14:55:54 +0530

    commit

--- DataAccessLayer/Migrations/20240419070338_v83.cs
+++ DataAccessLayer/Migrations/20240419070338_v83.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace DataAccessLayer.Migrations
 {
     /// <inheritdoc />
-    public partial class v80 : Migration
+    public partial class v83 : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
