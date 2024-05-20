0fbd210eff1033e6dfd19ea8ad9e3344e408b34e
YOGENDRA KUMAR <yogendrakumar8219@gmail.com>
2024-05-20 09:06:05 +0530

    commit

--- TestConsoleReso/obj/Release/net7.0/TestConsoleReso.AssemblyInfo.cs
+++ TestConsoleReso/obj/Release/net7.0/TestConsoleReso.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestConsoleReso")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2946182d1605ae2341d2913451555f53bf04cc1b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestConsoleReso")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestConsoleReso")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
