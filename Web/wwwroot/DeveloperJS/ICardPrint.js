47e1d0169ec6738fa76f343d8567ff08acb7b63d
YOGENDRA KUMAR <yogendrakumar8219@gmail.com>
2024-06-27 11:59:49 +0530

    commit

--- Web/wwwroot/DeveloperJS/ICardPrint.js
+++ Web/wwwroot/DeveloperJS/ICardPrint.js
@@ -1,10 +1,9 @@
 ﻿$(document).ready(function () {
-
-$("#btn-i-card-print").click(function () {
-    var datef2 = new Date();
-    $(".watermark").html($("#IpaddresGloble").html() + ' ' + DateFormateddMMyyyyhhmmss(datef2))
-    window.print();
-});
+    $("#btn-i-card-print").click(function () {
+        var datef2 = new Date();
+        $(".watermark").html($("#IpaddresGloble").html() + ' ' + DateFormateddMMyyyyhhmmss(datef2))
+        window.print();
+    });
 });
 function GetICardPrintPreviewByRequestId(RequestId) {
     var userdata =
