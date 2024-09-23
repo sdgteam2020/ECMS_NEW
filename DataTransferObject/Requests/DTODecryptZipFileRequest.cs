7b3f532e46c00e6365ad7ca552de1e3b5ad6ed6e
YOGENDRA KUMAR <yogendrakumar8219@gmail.com>
2024-09-23 17:24:21 +0530

    commit

---
+++ DataTransferObject/Requests/DTODecryptZipFileRequest.cs
@@ -0,0 +1,21 @@
+﻿using DataTransferObject.Localize;
+using DataTransferObject.Validation;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTransferObject.Requests
+{
+    public class DTODecryptZipFileRequest
+    {
+        [AllowedExtensions(new string[] { ".zip" })]
+        [AllowedContentType(new string[] { "application/zip" })]
+        [MaxFileSize(5120, "ZipFile")]
+        public IFormFile ZipFile { get; set; }
+        public string PrivateKey { get; set; } = string.Empty;
+    }
+}
