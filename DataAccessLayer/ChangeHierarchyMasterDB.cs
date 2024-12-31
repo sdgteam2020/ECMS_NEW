354530eed5c82b63db4e4098c78bf345c0bb4ef9
YOGENDRA KUMAR <yogendrakumar8219@gmail.com>
2024-12-31 09:34:34 +0530

    commit

--- DataAccessLayer/ChangeHierarchyMasterDB.cs
+++ DataAccessLayer/ChangeHierarchyMasterDB.cs
@@ -23,25 +23,45 @@ namespace DataAccessLayer
             _contextDP = dapperContext;
             _logger = logger;
         }
-        public async Task<int> UpdateChageComdByCorps(MapUnit Data)
+        public async Task<bool> UpdateChageComdByCorps(MapUnit Data)
         {
+            var (db, transaction) = _contextDP.CreateConnectionWithTransaction();
             try
             {
+                var parameters = new DynamicParameters();
+                parameters.Add("@CorpsId", Data.Corps.CorpsId, DbType.Byte, ParameterDirection.Input);
+                parameters.Add("@CorpsName", Data.Corps.CorpsName, DbType.String, ParameterDirection.Input, 20);
+                parameters.Add("@ComdId", Data.ComdId, DbType.Byte, ParameterDirection.Input);
+                parameters.Add("@IsActive", Data.Corps.IsActive, DbType.Boolean, ParameterDirection.Input);
+                parameters.Add("@Updatedby", Data.Corps.Updatedby, DbType.Int32, ParameterDirection.Input);
+                parameters.Add("@UpdatedOn", Data.Corps.UpdatedOn, DbType.DateTime, ParameterDirection.Input);
+
                 using (var connection = _contextDP.CreateConnection())
                 {
-                    connection.Execute("update MBde set ComdId=@ComdId where CorpsId=@CorpsId", new { comdId = Data.ComdId, corpsId = Data.CorpsId });
-                    connection.Execute("update MDiv set ComdId=@ComdId where CorpsId=@CorpsId", new { comdId = Data.ComdId, corpsId = Data.CorpsId });
-                    connection.Execute("update MapUnit set ComdId=@ComdId where CorpsId=@CorpsId", new { comdId = Data.ComdId, corpsId = Data.CorpsId });
+                    await db.ExecuteAsync("update MCorps set CorpsName=@CorpsName,ComdId=@ComdId,IsActive=@IsActive,Updatedby=@Updatedby,UpdatedOn=@UpdatedOn where CorpsId=@CorpsId", parameters, transaction: transaction);
+                    await db.ExecuteAsync("update MBde set ComdId=@ComdId where CorpsId=@CorpsId", new { comdId = Data.ComdId, corpsId = Data.CorpsId }, transaction: transaction);
+                    await db.ExecuteAsync("update MDiv set ComdId=@ComdId where CorpsId=@CorpsId", new { comdId = Data.ComdId, corpsId = Data.CorpsId }, transaction: transaction);
+                    await db.ExecuteAsync("update MapUnit set ComdId=@ComdId where CorpsId=@CorpsId", new { comdId = Data.ComdId, corpsId = Data.CorpsId }, transaction: transaction);
+
+                    // Commit the transaction if all operations succeed
+                    transaction.Commit();
+                    return true;
                 }
 
 
             }
             catch (Exception ex)
             {
-
+                // Rollback the transaction if any operation fails
+                transaction.Rollback();
+                _logger.LogError(1001, ex, "ChangeHierarchyMasterDB->UpdateChageComdByCorps");
+                return false;
             }
-
-            return 1;
+            finally
+            {
+                // Dispose of the connection
+                db.Dispose();
+            }
         }
         public async Task<bool> UpdateComdCorpsByDivs(MapUnit Data)
         {
@@ -49,16 +69,16 @@ namespace DataAccessLayer
             try
             {
                 var parameters = new DynamicParameters();
-                parameters.Add("@BdeId", Data.Bde.BdeId, DbType.Byte, ParameterDirection.Input);
-                parameters.Add("@BdeName", Data.Bde.BdeName, DbType.String, ParameterDirection.Input,20);
+                parameters.Add("@DivId", Data.Div.DivId, DbType.Byte, ParameterDirection.Input);
+                parameters.Add("@DivName", Data.Div.DivName, DbType.String, ParameterDirection.Input,20);
                 parameters.Add("@ComdId", Data.ComdId, DbType.Byte, ParameterDirection.Input);
                 parameters.Add("@CorpsId", Data.CorpsId, DbType.Byte, ParameterDirection.Input);
                 parameters.Add("@DivId", Data.DivId, DbType.Byte, ParameterDirection.Input);
-                parameters.Add("@IsActive", Data.IsActive, DbType.Boolean, ParameterDirection.Input);
-                parameters.Add("@Updatedby", Data.Updatedby, DbType.Int32, ParameterDirection.Input);
-                parameters.Add("@UpdatedOn", Data.UpdatedOn, DbType.DateTime, ParameterDirection.Input);
+                parameters.Add("@IsActive", Data.Div.IsActive, DbType.Boolean, ParameterDirection.Input);
+                parameters.Add("@Updatedby", Data.Div.Updatedby, DbType.Int32, ParameterDirection.Input);
+                parameters.Add("@UpdatedOn", Data.Div.UpdatedOn, DbType.DateTime, ParameterDirection.Input);
                 
-                await db.ExecuteAsync("update MBde set BdeName=@BdeName,ComdId=@ComdId,CorpsId=@CorpsId,DivId=@DivId,IsActive=@IsActive,Updatedby=@Updatedby,UpdatedOn=@UpdatedOn where BdeId=@BdeId", parameters, transaction: transaction);
+                await db.ExecuteAsync("update MDiv set DivName=@DivName,ComdId=@ComdId,CorpsId=@CorpsId,IsActive=@IsActive,Updatedby=@Updatedby,UpdatedOn=@UpdatedOn where DivId=@DivId", parameters, transaction: transaction);
                 await db.ExecuteAsync("update MBde set ComdId=@ComdId,CorpsId=@CorpsId where DivId=@DivId", new { comdId = Data.ComdId, corpsId = Data.CorpsId, divId = Data.DivId },transaction:transaction);
                 await db.ExecuteAsync("update MapUnit set ComdId=@ComdId,CorpsId=@CorpsId where DivId=@DivId", new { comdId = Data.ComdId, corpsId = Data.CorpsId, divId = Data.DivId }, transaction: transaction);
                 // Commit the transaction if all operations succeed
@@ -78,21 +98,41 @@ namespace DataAccessLayer
                 db.Dispose();
             }
         }
-        public async Task<int> UpdateComdCorpsDivsBybdes(MapUnit Data)
+        public async Task<bool> UpdateComdCorpsDivsBybdes(MapUnit Data)
         {
+            var (db, transaction) = _contextDP.CreateConnectionWithTransaction();
             try
             {
-                using (var connection = _contextDP.CreateConnection())
-                {
-                   
-                    connection.Execute("update MapUnit set ComdId=@ComdId,CorpsId=@CorpsId,DivId=@DivId where BdeId=@BdeId", new { comdId = Data.ComdId, corpsId = Data.CorpsId, divId = Data.DivId, bdeId=Data.BdeId });
-                }
+                var parameters = new DynamicParameters();
+                parameters.Add("@BdeId", Data.Bde.BdeId, DbType.Byte, ParameterDirection.Input);
+                parameters.Add("@BdeName", Data.Bde.BdeName, DbType.String, ParameterDirection.Input, 20);
+                parameters.Add("@ComdId", Data.ComdId, DbType.Byte, ParameterDirection.Input);
+                parameters.Add("@CorpsId", Data.CorpsId, DbType.Byte, ParameterDirection.Input);
+                parameters.Add("@DivId", Data.DivId, DbType.Byte, ParameterDirection.Input);
+                parameters.Add("@IsActive", Data.Bde.IsActive, DbType.Boolean, ParameterDirection.Input);
+                parameters.Add("@Updatedby", Data.Bde.Updatedby, DbType.Int32, ParameterDirection.Input);
+                parameters.Add("@UpdatedOn", Data.Bde.UpdatedOn, DbType.DateTime, ParameterDirection.Input);
 
+                await db.ExecuteAsync("update MBde set BdeName=@BdeName,ComdId=@ComdId,CorpsId=@CorpsId,DivId=@DivId,IsActive=@IsActive,Updatedby=@Updatedby,UpdatedOn=@UpdatedOn where BdeId=@BdeId", parameters, transaction: transaction);
+                await db.ExecuteAsync("update MapUnit set ComdId=@ComdId,CorpsId=@CorpsId,DivId=@DivId where BdeId=@BdeId", new { comdId = Data.ComdId, corpsId = Data.CorpsId, divId = Data.DivId, bdeId = Data.BdeId });
+
+                // Commit the transaction if all operations succeed
+                transaction.Commit();
+                return true;
 
             }
-            catch (Exception ex) { }
-
-            return 1;
+            catch (Exception ex)
+            {
+                // Rollback the transaction if any operation fails
+                transaction.Rollback();
+                _logger.LogError(1001, ex, "ChangeHierarchyMasterDB->UpdateComdCorpsByDivs");
+                return false;
+            }
+            finally
+            {
+                // Dispose of the connection
+                db.Dispose();
+            }
         }
     }
 }
