5f2e4f8d4d9c1804a978511c87d40d5c2de92dae
YOGENDRA KUMAR <yogendrakumar8219@gmail.com>
2025-03-12 10:45:33 +0530

    after both merged then issue fixed

--- BusinessLogicsLayer/Account/IAccountBL.cs
+++ BusinessLogicsLayer/Account/IAccountBL.cs
@@ -12,7 +12,6 @@ namespace BusinessLogicsLayer.Account
         public Task<DTOAccountResponse?> FindDomainId(string DomainId);
         public Task<bool?> FindRoleByName(string Role);
         public Task<List<DTORegisterListRequest>?> DomainApproveList();
-        public Task<List<DTOProfileManageResponse>?> GetAllProfileManage(string Search, string Choice);
         public Task<DTODataTablesResponse<DTOUserRegnResponse>> GetAllUserRegn(DTODataTablesRequest request);
         public Task<DTODataTablesResponse<DTODomainRegnResponse>> GetAllDomainRegn(DTODataTablesRequest request);
         public Task<DTODataTablesResponse<DTOProfileManageResponse>> GetAllProfileManage(DTODataTablesRequest request);
