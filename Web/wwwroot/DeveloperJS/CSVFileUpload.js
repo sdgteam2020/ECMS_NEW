8bd81eb3e5f4eb72b37e96f1f5a1b5234436a57b
kapoor <kapoor2q@gmail.com>
2025-04-24 15:14:59 +0530

    commit

--- Web/wwwroot/DeveloperJS/CSVFileUpload.js
+++ Web/wwwroot/DeveloperJS/CSVFileUpload.js
@@ -34,8 +34,8 @@ function validateCsvFileOnChange() {
             toastr.error('The selected file is empty.');
             return;
         }
-
-        if (lines.length <= 2) {
+       
+        if (lines.length < 2) {
             toastr.error('The CSV file must contain at least 1 data row.');
             return;
         }
