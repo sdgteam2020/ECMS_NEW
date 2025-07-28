142bb78ce4e2bbd27f44841ef87615c90ad0db52
tezpratapsingh1997 <tezpratapsingh384@gmail.com>
2025-07-28 11:35:34 +0530

    Add migration for Approved column in MAppointment
    Introduced a new migration class `AddApprovedToMAppointment` to add an `Approved` column of type `smallint` to the `MAppointment` table with a default value of 0. The migration includes methods to apply and revert the change.

---
+++ DataAccessLayer/Migrations/20250724042223_AddApprovedToMAppointment.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddApprovedToMAppointment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<short>(
+                name: "Approved",
+                table: "MAppointment",
+                type: "smallint",
+                nullable: false,
+                defaultValue: (short)0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Approved",
+                table: "MAppointment");
+        }
+    }
+}
