316fc3e310524ec904dde03d63d853762e59bc4e
YOGENDRA KUMAR <yogendrakumar8219@gmail.com>
2025-08-27 11:02:27 +0530

    Public Key and Private Key stored in MEncryptionSetting Table.

--- DataTransferObject/Domain/Master/MEncryptionSetting.cs
+++ DataTransferObject/Domain/Master/MEncryptionSetting.cs
@@ -1,7 +1,8 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,5 +16,11 @@ namespace DataTransferObject.Domain.Master
         public int Id { get; set; }  // Primary Key
         public required byte[] KeyBytes { get; set; }  // 256-bit key (32 bytes)
         public required byte[] IVBytes { get; set; }  // 128-bit IV (16 bytes)
+
+        [Unicode(false)]
+        public string PublicKey { get; set; } = string.Empty;
+
+        [Unicode(false)]
+        public string PrivateKey { get; set; } = string.Empty;
     }
 }
--- Web/Controllers/BasicDetailController.cs
+++ Web/Controllers/BasicDetailController.cs
@@ -8,6 +8,7 @@ using BusinessLogicsLayer.DestructionCard;
 using BusinessLogicsLayer.DispatchCard;
 using BusinessLogicsLayer.DispatchCardMapping;
 using BusinessLogicsLayer.DistributeCard;
+using BusinessLogicsLayer.EncryptionSetting;
 using BusinessLogicsLayer.FaultyCard;
 using BusinessLogicsLayer.HotlistCard;
 using BusinessLogicsLayer.LostCard;
@@ -101,13 +102,14 @@ namespace Web.Controllers
         private readonly IDispatchCardBL dispatchCardBL;
         private readonly IDispatchCardMappingBL dispatchCardMappingBL;
         private readonly IImageEncryptAndDecrypt imageEncryptAndDecrypt;
+        private readonly IEncryptionSettingBL encryptionSettingBL;
 
         public BasicDetailController(IConfiguration configuration, IBasicDetailBL basicDetailBL, IMapUnitBL mapUnitBL, IBasicDetailTempBL basicDetailTempBL, IService service, IMapper mapper,
             UserManager<ApplicationUser> userManager, IWebHostEnvironment hostingEnvironment, IDataProtectionProvider dataProtectionProvider,
                               DataProtectionPurposeStrings dataProtectionPurposeStrings, ILogger<BasicDetailController> logger, IStepCounterBL iStepCounterBL,
                               ITrnFwnBL iTrnFwnBL, ITrnICardRequestBL iTrnICardRequestBL, IDomainMapBL iDomainMapBL
             , IBasicUploadBL basicUploadBL, IBasicAddressBL basicAddressBL, IBasicinfoBL basicinfoBL, IRankBL rankBL, INotificationBL notificationBL, IMasterBL masterBL
-           , ITrnLoginLogBL iTrnLoginLogBL, IICardHoldBL iICardHoldBL, IcsvImportBl iCSVImportBL, IFaultyCardBL _faultyCardBL, IHotlistCardBL hotlistCardBL, ILostCardBL lostCardBL, IDistributeCardBL distributeCardBL, IDestructionCardBL destructionCardBL, IDispatchCardBL dispatchCardBL, IDispatchCardMappingBL dispatchCardMappingBL, IImageEncryptAndDecrypt imageEncryptAndDecrypt)
+           , ITrnLoginLogBL iTrnLoginLogBL, IICardHoldBL iICardHoldBL, IcsvImportBl iCSVImportBL, IFaultyCardBL _faultyCardBL, IHotlistCardBL hotlistCardBL, ILostCardBL lostCardBL, IDistributeCardBL distributeCardBL, IDestructionCardBL destructionCardBL, IDispatchCardBL dispatchCardBL, IDispatchCardMappingBL dispatchCardMappingBL, IImageEncryptAndDecrypt imageEncryptAndDecrypt, IEncryptionSettingBL encryptionSettingBL)
         {
             _configuration = configuration;
             this.basicDetailBL = basicDetailBL;
@@ -144,6 +146,7 @@ namespace Web.Controllers
             this.dispatchCardBL = dispatchCardBL;
             this.dispatchCardMappingBL = dispatchCardMappingBL;
             this.imageEncryptAndDecrypt= imageEncryptAndDecrypt;
+            this.encryptionSettingBL = encryptionSettingBL;
         }
 
         #region Index/ApprovalForIO/View/InaccurateData/InaccurateDataView/RequestType
@@ -1726,7 +1729,16 @@ namespace Web.Controllers
         {
             try
             {
-                model.PrivateKey = _configuration["Key:PrivateKey"];
+                var keyRecord = await encryptionSettingBL.Get(1);
+                if (keyRecord != null)
+                {
+                    model.PrivateKey = keyRecord.PrivateKey;
+                }
+                else
+                {
+                    throw new InvalidOperationException("Encryption key record not found.");
+                }
+                
                 if (ModelState.IsValid)
                 {
                     string sourceFolderPhotoPhy = Convert.ToString(Path.Combine(hostingEnvironment.WebRootPath, "WriteReadData", "ExportAFSACCell", "Temp"));
@@ -2186,10 +2198,23 @@ namespace Web.Controllers
         {
             try
             {
+                #region Value
                 //Data.publicKey = "MIIBCgKCAQEArhSYCF6ie0rkkXe2HSqKXQ/Sa/NwwbXQ/q1sEEL2eWGnpCa0+49DtRWtybLfK6A51Cj1TX2HnOGuPROQ46DOPI6giwDXnIimHeHAMCd4GqFuDAlDytFNls4XHCMxt1Ql2nVWVxBc2DSTGB35H+eT06rgL+j6ra0iaorAnghUzgIsgH8uLoXX9WqQZXI3rZcH6483ymh0fs/6hS0L5D/pNSaAIuMse3Jg6vcv5z/M7ZzTfiKHO0XkZE/qkm6hIR8uHi4jJwoCdHJ4Fc0wZ+ekd3h/Z2nNXbim07jX6ZcoKL5udYf5u0iFqplg6ao+qssiHF4RMCeDh1vBU5vkSpyUEQIDAQAB";
                 //Data.privateKey = "MIIEpgIBAAKCAQEArhSYCF6ie0rkkXe2HSqKXQ/Sa/NwwbXQ/q1sEEL2eWGnpCa0+49DtRWtybLfK6A51Cj1TX2HnOGuPROQ46DOPI6giwDXnIimHeHAMCd4GqFuDAlDytFNls4XHCMxt1Ql2nVWVxBc2DSTGB35H+eT06rgL+j6ra0iaorAnghUzgIsgH8uLoXX9WqQZXI3rZcH6483ymh0fs/6hS0L5D/pNSaAIuMse3Jg6vcv5z/M7ZzTfiKHO0XkZE/qkm6hIR8uHi4jJwoCdHJ4Fc0wZ+ekd3h/Z2nNXbim07jX6ZcoKL5udYf5u0iFqplg6ao+qssiHF4RMCeDh1vBU5vkSpyUEQIDAQABAoIBAQCDDhgDPRPAFHsNlP1y6cLvGulEwiqiezoTcgZIG9GpQj7OUyGvvYSwwNhsYBCprF+8/PToWNgO4MynSKKs7DQ33Py6iXDJdQrytjFVT3GZQu0xfIwgFgD+xrsZQNm99kjlNa9BrpznXHVdE7upLFPbZ+qNxy1qMU0Wvs0SbJ1D1ZruXtbRqbOKzryZKa5NpboDBXIPw/o9RZS8eTFVl1SZC6asrokEepVWUsMwg/yORvKf/p/cCZBjbKQ+oclsT5ljht5j53YuYlixIYJNghmniMMEWwuyfeKZ5swL0HbGTJvkrz55mWKP7NtWGIIUzhMltPef6LNcjeMw/SOvTghNAoGBAMmSwbWRmJfXzAuq/UnnUoi8zpJuoWHh8fww0w0/bOuuVGkk/0Z+LXaWOeSRFjrwT1UU+uSW5Lj0bTRJHeGCxwaA8d2CJMUlPCBx6xukFDyaCZavtwxUMC5hOLp7DvCWyMZqQP5UAI5ukMYgljE9rvTfpXQBp04QH3xYCjXiUPffAoGBAN0VdxY+uvd7roiW1JamrzeyDCkIlWGriUd+WoO6KVKGM7Gi1E7oZcaSW9BC/qutRBuuuOFfu3btC3BlBbieXXAztAvEPD8e/JvE5FSpkcY8rELlC9Y0M3hxdJoMWH/tIwJIVKsxnGzCfRemMjvLiAGc1YSWnl5lslpQSrlJG7IPAoGBAMCXmL87ijliNRHc4L7w5vnAs/pS+5zDPerAV5ZryEzytrHzaHhY7GVGqa/KNBxCKPpY3lL0HTreR0zSo1spEbIUF4OV6j33EpjJX2J8hd1VK94uq017TsGxoHsEQsT6vIBfWxPk/NcZqveygO4xSm2rFbFeNxUt8HdkwvSy9LuvAoGBAL/W9HMVE9/ULurPFsFy+e/2S57/l8AcvQ6QkbJkQ58cXJbzmA6wkj/wmELrH1mRC9yJjFvkWiMkJhztTD2bDbFi7ASZzz1mggQYoZjlW10NIN0bK15ABbmpmWhi9hhriUldwjqa3gVx7mIrEMPaJLZhhNV8bQe0b0L3ESAeVC35AoGBAIFUQ9VziGZ2UMrDxMPU2AoMqfJe3X82CcUu/WS3KntAObSlSA3Od2Ow8gHs6KtVxMYLND9nHJ+WXMXASbv/ou1E/h8lRvg7OjFEnscgz8w5Kvf5egIoYFoMAg7TA8e/8mZ8NIli88T2/vvMZHhUSrRm43cssViI1kLFXfywzzOX";
-                Data.publicKey = _configuration["Key:PublicKey"];
-                Data.privateKey = _configuration["Key:PrivateKey"];
+                //Data.publicKey = _configuration["Key:PublicKey"];
+                //Data.privateKey = _configuration["Key:PrivateKey"];
+                #endregion
+
+                var keyRecord = await encryptionSettingBL.Get(1);
+                if (keyRecord != null)
+                {
+                    Data.publicKey = keyRecord.PublicKey;
+                    Data.privateKey = keyRecord.PrivateKey;
+                }
+                else
+                {
+                    throw new InvalidOperationException("Encryption key record not found.");
+                }
 
                 DTOApplFwdConditionRequest? dTOApplFwdCondition = _configuration.GetSection("ApplFwdCondition").Get<DTOApplFwdConditionRequest>() ?? new DTOApplFwdConditionRequest
                 {
