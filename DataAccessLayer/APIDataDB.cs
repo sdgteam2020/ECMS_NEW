21a935981d0cb73e497160a94fb9b9adfd628591
tezpratapsingh1997 <tezpratapsingh384@gmail.com>
2025-09-02 12:02:53 +0530

    Added XML and inline comments to GetByoffrsIC method to explain the SQL query, decryption logic, and response mapping

--- DataAccessLayer/APIDataDB.cs
+++ DataAccessLayer/APIDataDB.cs
@@ -149,43 +149,56 @@ namespace DataAccessLayer
         }
 
 
+        /// <summary>
+        /// Asynchronously retrieves personal data for the given Army number (Pers_Army_No) from the MApiDataOffrs table, 
+        /// decrypts the sensitive fields, and returns a DTOApiPersDataResponse with the data.
+        /// </summary>
+        /// <param name="Data">The DTOAPIDataRequest object containing the Army number for querying.</param>
+        /// <returns>
+        /// A DTOApiPersDataResponse object containing the personal data if the Army number exists, 
+        /// otherwise returns a response with a message indicating the Army number was not found.
+        /// </returns>
         public async Task<DTOApiPersDataResponse> GetByoffrsIC(DTOAPIDataRequest Data)
         {
-            //string query = "SELECT [ApplyForId],[Pers_Army_No],[Pers_Blood_Gp],[Pers_District],[Pers_Father_Name],[Pers_Gender],[Pers_Height],[Pers_House_no],[Pers_Iden_mark_1],[Pers_Iden_mark_2],[Pers_Moh_st],[Pers_Pin_code],[Pers_Police_stn],[Pers_Post_office],[Pers_Rank],[Pers_Regt],[Pers_State],[Pers_Tehsil],[Pers_UID],[Pers_Village],[Pers_birth_dt],[Pers_enrol_dt],[Pers_name] FROM [dbo].[MApiDataOffrs] where [Pers_Army_No]=@ArmyNo";
-            //    string query = "SELECT     [ApplyForId],[Pers_Army_No] "+
-            //" FROM [dbo].MApiDataOffrs where [Pers_Army_No]=@ArmyNo";
-            string query = "SELECT     [ApplyForId],[Pers_Army_No],CONVERT(nvarchar(MAX),DEcryptByPassPhrase('ASDC@123',[Pers_name])) [Pers_name],[Pers_Rank],CONVERT(nvarchar(MAX),DEcryptByPassPhrase('ASDC@123',[Pers_Father_Name])) [Pers_Father_Name],CONVERT(nvarchar(MAX),DEcryptByPassPhrase('ASDC@123',[Pers_birth_dt])) [Pers_birth_dt] " +
-                            " ,[Pers_enrol_dt] " +
-                            " ,CONVERT(nvarchar(MAX),DEcryptByPassPhrase('ASDC@123',[Pers_District])) [Pers_District]" +
-                            " ,CONVERT(nvarchar(MAX),DEcryptByPassPhrase('ASDC@123',[Pers_State])) [Pers_State]" +
-                            " ,[Pers_Regt]" +
-                            " ,[Pers_Height]" +
-                            " ,CONVERT(nvarchar(MAX),DEcryptByPassPhrase('ASDC@123',[Pers_UID])) [Pers_UID]" +
-                            " ,[Pers_Blood_Gp]" +
-                            " ,CONVERT(nvarchar(MAX),DEcryptByPassPhrase('ASDC@123',[Pers_House_no])) [Pers_House_no]" +
-                            " ,[Pers_Moh_st]" +
-                            " ,CONVERT(nvarchar(MAX),DEcryptByPassPhrase('ASDC@123',[Pers_Village])) [Pers_Village]" +
-                            " ,CONVERT(nvarchar(MAX),DEcryptByPassPhrase('ASDC@123',[Pers_Tehsil])) [Pers_Tehsil]" +
-                            " ,CONVERT(nvarchar(MAX),DEcryptByPassPhrase('ASDC@123',[Pers_Post_office])) [Pers_Post_office]" +
-                            " ,[Pers_Police_stn]" +
-                            " ,[Pers_Pin_code]" +
-                            " ,[Pers_Iden_mark_1]" +
-                            " ,[Pers_Iden_mark_2]" +
-                            " ,[Pers_Gender]" +
-                            " FROM [dbo].[MApiDataOffrs] where [Pers_Army_No]=@ArmyNo";
+            // SQL query to select personal data from the MApiDataOffrs table for the given Army number.
+            // Sensitive fields such as name, father name, birth date, etc., are decrypted using the DEcryptByPassPhrase function.
+            string query = "SELECT [ApplyForId], [Pers_Army_No], " +
+                           "CONVERT(nvarchar(MAX), DEcryptByPassPhrase('ASDC@123', [Pers_name])) [Pers_name], " +
+                           "CONVERT(nvarchar(MAX), DEcryptByPassPhrase('ASDC@123', [Pers_Father_Name])) [Pers_Father_Name], " +
+                           "CONVERT(nvarchar(MAX), DEcryptByPassPhrase('ASDC@123', [Pers_birth_dt])) [Pers_birth_dt], " +
+                           "[Pers_enrol_dt], " +
+                           "CONVERT(nvarchar(MAX), DEcryptByPassPhrase('ASDC@123', [Pers_District])) [Pers_District], " +
+                           "CONVERT(nvarchar(MAX), DEcryptByPassPhrase('ASDC@123', [Pers_State])) [Pers_State], " +
+                           "[Pers_Regt], [Pers_Height], " +
+                           "CONVERT(nvarchar(MAX), DEcryptByPassPhrase('ASDC@123', [Pers_UID])) [Pers_UID], " +
+                           "[Pers_Blood_Gp], " +
+                           "CONVERT(nvarchar(MAX), DEcryptByPassPhrase('ASDC@123', [Pers_House_no])) [Pers_House_no], " +
+                           "[Pers_Moh_st], " +
+                           "CONVERT(nvarchar(MAX), DEcryptByPassPhrase('ASDC@123', [Pers_Village])) [Pers_Village], " +
+                           "CONVERT(nvarchar(MAX), DEcryptByPassPhrase('ASDC@123', [Pers_Tehsil])) [Pers_Tehsil], " +
+                           "CONVERT(nvarchar(MAX), DEcryptByPassPhrase('ASDC@123', [Pers_Post_office])) [Pers_Post_office], " +
+                           "[Pers_Police_stn], [Pers_Pin_code], [Pers_Iden_mark_1], [Pers_Iden_mark_2], [Pers_Gender] " +
+                           "FROM [dbo].[MApiDataOffrs] WHERE [Pers_Army_No] = @ArmyNo";
 
+            // Using the database connection to execute the query and retrieve the personal data for the given Army number.
             using (var connection = _contextDP.CreateConnection())
             {
+                // Execute the query asynchronously and retrieve the first matching record (if any).
                 MApiDataOffrs? ret = (await connection.QueryAsync<MApiDataOffrs>(query, new { Data.ArmyNo })).FirstOrDefault();
+
+                // Create a response object to store the result.
                 DTOApiPersDataResponse response = new DTOApiPersDataResponse();
 
+                // If a record was found, populate the response object with the decrypted data.
                 if (ret != null)
                 {
                     response.Pers_Army_No = ret.Pers_Army_No ?? string.Empty;
                     response.Pers_name = ret.Pers_name ?? string.Empty;
                     response.Pers_birth_dt = ret.Pers_birth_dt ?? string.Empty;
                     response.Pers_enrol_dt = ret.Pers_enrol_dt ?? string.Empty;
-                    response.Pers_Address.Pers_House_no = ret.Pers_House_no ?? string.Empty; ;
+
+                    // Mapping address-related data
+                    response.Pers_Address.Pers_House_no = ret.Pers_House_no ?? string.Empty;
                     response.Pers_Address.Pers_Moh_st = ret.Pers_Moh_st;
                     response.Pers_Address.Pers_Village = ret.Pers_Village;
                     response.Pers_Address.Pers_Tehsil = ret.Pers_Tehsil;
@@ -194,17 +207,23 @@ namespace DataAccessLayer
                     response.Pers_Address.Pers_Pin_code = ret.Pers_Pin_code;
                     response.Pers_Address.Pers_District = ret.Pers_District;
                     response.Pers_Address.Pers_State = ret.Pers_State;
+
+                    // Status and message indicating success
                     response.Status = true;
                     response.Message = "Ok";
                 }
-                else 
+                else
                 {
+                    // If no matching record is found, set status and message accordingly
                     response.Status = false;
                     response.Message = "Army No not found!";
                 }
+
+                // Return the response object.
                 return response;
             }
         }
+
     }
 }
 
